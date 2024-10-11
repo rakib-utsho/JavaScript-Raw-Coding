cadbda22287126d755707b01c52f3d37da54c91e
rakib-utsho <rakibutsho1920@gmail.com>
2024-10-12 02:16:59 +0600

    new practice

---
+++ practice/prac2.js
@@ -0,0 +1,18 @@
+// practice 1
+function printPome() {
+    console.log(" Twinkle, twinkle, little Star, \n How I wonder what you are! \n Up above the world so high, \n Like a diamond in the sky. \n When the blazing sun is gone, \n When he nothing shines upon, \n Then you show your little light, \n Twinkle, twinkle, all the night.");
+    console.log("\n");
+}
+printPome();
+
+// complex logic function 
+function diceRoll() {
+    let dice = Math.floor(Math.random()*6)+1;
+    console.log(dice);
+    console.log("\n");
+}
+
+diceRoll();
+diceRoll();
+diceRoll();
+diceRoll();
