4eaa8f6c827a75284d3391a63076d8532b062515
rakib-utsho <rakibutsho1920@gmail.com>
2024-10-12 01:43:42 +0600

    regular short practice

---
+++ practice/prac1.js
@@ -0,0 +1,24 @@
+// dice roll
+
+const dice = Math.floor(Math.random() * 6)+1;
+console.log(dice);
+
+// object 1
+
+let car = {
+    name: "BMW",
+    model: 2016,
+    color: "red"
+};
+console.log(car.name);
+
+// object 2
+
+let person = {
+    name: "Abu Talha",
+    age: 23,
+    city: "Dhaka"
+};
+person.city = "New York";
+person.country = "United state";
+console.log(person);
