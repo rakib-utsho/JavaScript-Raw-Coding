1cfd95a730f6bab734ed15b3417c0df4a4140065
rakib-utsho <rakibutsho1920@gmail.com>
2024-10-12 21:59:20 +0600

    update

--- practice/prac2.js
+++ practice/prac2.js
@@ -36,3 +36,38 @@ function printTable(n) {
 }
 
 printTable();
+
+
+// Qs1. Write a JavaScript function that returns array elements larger than a number.
+
+let arr = [8,9,10,1,2,3,4,5,6,7];
+let num = 5;
+
+function getElement(arr, num){
+    for (let i=0; i<arr.length;i++){
+        if (arr[i]>num){
+            console.log(arr[i]);
+        }
+    }
+}
+
+getElement(arr,num);
+
+// Qs2. Write a JavaScript function to extract unique characters from a string. Example: str = “abcdabcdefgggh” ans = “abcdefgh”
+
+let str = "abcdabcdefgggh";
+//function to get String with all unique elements
+function getUnique(str) {
+    let ans = "";
+
+    for(let i = 0; i<str.length; i++){
+        let currChar = str[i];
+        if (ans.indexOf(currChar)==-1){
+        //if current character is not added, then add it in ans.
+//Otherwise it is a duplicate. 
+            ans+=currChar;
+        }
+    }
+    return ans;
+}
+getUnique(str);
