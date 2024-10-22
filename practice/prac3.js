7183480a296fe9c005dec9a5140d76cb1405308a
rakib-utsho <rakibutsho1920@gmail.com>
2024-10-23 03:28:25 +0600

    practice 3

--- practice/prac3.js
+++ practice/prac3.js
@@ -12,15 +12,15 @@ let arr = [1,2,3,4,5,6];
 console.log(arrayAverage(arr));
 
 // Part 7 Q2
-let num = 0;
-const isEven = (num) => num%2 == 0;
+let numb = 0;
+const isEven = (numb) => numb%2 == 0;
 
 // multiple of 10
 
-let nums = [10, 20, 30, 40, 50, 60, 70];
+let numss = [10, 20, 30, 40, 50, 60, 70];
 
 
-let ans = nums.every((el)=>el%10==0);
+let ans = numss.every((el)=>el%10==0);
 
 console.log(ans);
 
@@ -39,4 +39,28 @@ function getMin(nums){
     return min;
 };
 
+
+// prac 8 q1
+
+let numa = [1, 2, 3, 4, 5];
+
+const square = numa.map((numa) => numa*numa);
+console.log(square); 
+
+let sum = square.reduce((acc, cur) => acc+cur, 0);
+console.log(sum);
+
+let avg = sum/numa.length;
+console.log(avg);
+
+
+// prac 8 q2
+
+let numArr = [2, 4, 6, 8, -1, -4];
+console.log(numArr.map((numArr) => numArr +5));
+
+// prac 8 q3
+
+let str = ["sohan", "naime", "shahadat", "najmul", "sabbir"];
+console.log(str.map((str)=>str.toUpperCase()));
 
