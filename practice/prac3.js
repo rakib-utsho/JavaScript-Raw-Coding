1be1e557b34eaa8749d1fedbd4c2f25b19564868
rakib-utsho <rakibutsho1920@gmail.com>
2024-10-23 02:35:48 +0600

    practice 3

--- practice/prac3.js
+++ practice/prac3.js
@@ -10,3 +10,33 @@ const arrayAverage = (arr) => {
 };
 let arr = [1,2,3,4,5,6];
 console.log(arrayAverage(arr));
+
+// Part 7 Q2
+let num = 0;
+const isEven = (num) => num%2 == 0;
+
+// multiple of 10
+
+let nums = [10, 20, 30, 40, 50, 60, 70];
+
+
+let ans = nums.every((el)=>el%10==0);
+
+console.log(ans);
+
+// min number of array
+
+let nums = [10, 20, 30, 40, 50, 60, 7, 40, 2, 50, 1, 100];
+
+function getMin(nums){
+    let min = nums.reduce((min, el) => {
+        if (el>min){
+            return min;
+        }else{
+            return el;
+        }
+    });
+    return min;
+};
+
+
